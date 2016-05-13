18f4850fbf6aff9cd77831b84374434a63e9efb5
Jonathan Fielding <j.fielding@me.com>
2016-05-13 14:50:44 +0100

    add gzip support to rev

--- src/rev.js
+++ src/rev.js
@@ -3,16 +3,19 @@
 const gulp = require('gulp');
 const rev = require('gulp-rev');
 const revCssUrls = require('gulp-rev-css-url');
+const gulpGzip = require('gulp-gzip');
 
 module.exports = ({
     src = null,
     dest = null,
-    manifest = null
+    manifest = null,
+    gzip = false
 }) => {
     return () => {
         return gulp.src(src)
             .pipe(rev())
             .pipe(revCssUrls())
+            .pipe(gulpif(gzip, gulpGzip())
             .pipe(gulp.dest(dest))
             .pipe(rev.manifest())
             .pipe(gulp.dest(manifest));
